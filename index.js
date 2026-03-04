199acb67500e22e92bfeb526f3b4ad30c91414f6
DoreAupp <2024553te@aupp.edu.kh>
2026-03-04 15:15:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("API is running ✅"));
+app.get("/", (req, res) => res.send("API is running, yessirrrr 🫨"));
 
 app.get("/api/items", (req, res) => {
   res.json([
